494c99b9619554872bdfdb8654a127aaed9b523b
RBOLDI <Rowan.Bolding@hva.nl>
2019-01-17 14:42:21 +0100

    monasca agent update

--- Monasca_agent/agent.py
+++ Monasca_agent/agent.py
@@ -6,15 +6,15 @@ import psutil
 SHUNT_OHMS = 0.1
 MAX_EXPECTED_AMPS = 2.0
 ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS)
-ina.configure(ina.RANGE_16V)
+ina.configure(ina.RANGE_16V())
 
 REQUEST_TIME = Summary('request_processing_seconds',
 'Time spent processing request')
 
 PowerMeasurement = ina.power()
-CurrentMeasurement = abs(ina.current()
+CurrentMeasurement = abs(ina.current())
 
 if __name__ == '__main__':
 	while True:
-	post_metrics(self, PowerMeasurement)
-	post_metrics(self, CurrentMeasurement)
+		post_metrics(self, PowerMeasurement)
+		post_metrics(self, CurrentMeasurement)
