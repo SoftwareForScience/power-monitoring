3ff4510feed79e99becdb2b0efcd035bf6c5d83b
RPI nootje <nootje@twizzel.net>
2019-03-25 14:32:39 +0000

    WIP new setup

--- init.sh
+++ init.sh
@@ -7,17 +7,19 @@ sudo chown pi:pi -R ../.ssh
 sudo chown pi:pi -R ../docker-cluster
 
 # Add SSH-key to system
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
+#eval $(ssh-agent -s)
+#ssh-add ~/.ssh/id_rsa
 
 # DNS fix
 sudo cp dhcpcd.conf /etc/dhcpcd.conf
 sudo chmod 664 /etc/dhcpcd.conf
 sudo chown root:netdev /etc/dhcpcd.conf
+sudo systemctl daemon-reload
 sudo service dhcpcd restart
 
 # Sorry, we need git
 sudo apt-get install -y git
+git config core.sshCommand 'ssh -o StrictHostKeyChecking=no'
 git pull origin master
 
 # Now we gonna setup something...
