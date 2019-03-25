8be9f66b38d56446afc37645ca71402e8d78f4a2
Nootje <nootje@twizzel.net>
2019-03-25 15:18:21 +0000

    Back 1 setup file

--- setup.sh
+++ setup.sh
@@ -15,6 +15,27 @@ sudo apt-get -y autoremove
 sudo apt-get install -y git
 sudo apt-get install -y python-pip
 
-# Reboot now...
-echo "REBOOT THIS THING! (test)"
+
+# Install docker
+sudo chmod +x get-docker.sh
+sudo ./get-docker.sh
+
+# Install python docker + futures
+sudo pip install docker
+sudo pip install futures
+
+# Build Docker Image
+sudo docker build -t twizzel/clustermd5sum docker-image
+
+# Make Docker API public
+#sudo mkdir /etc/systemd/system/docker.service.d/
+#sudo cp override.conf /etc/systemd/system/docker.service.d/override.conf
+#sudo chown root:root /etc/systemd/system/docker.service.d/override.conf
+
+# Reload Docker Daemon
+#sudo systemctl daemon-reload
+#sudo systemctl restart docker
+
+# Yeahhh... Some sneaky reference ;-)
+echo "SETUP DONE! - Made by Twizzel"
 
