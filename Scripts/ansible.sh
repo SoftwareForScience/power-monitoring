c61471d5ae948b8360ef5645658a528ed724127e
Cas <c.vanderlaan@viriciti.com>
2019-06-12 22:57:02 +0200

    Fix more files

--- Scripts/ansible.sh
+++ Scripts/ansible.sh
@@ -1,1 +1,21 @@
-/home/cas/SfS/Ansible/CorrectAnsible/ansible.sh
+#!/bin/bash
+
+# Upgrade packages
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+
+# Git & PIP
+sudo apt-get install -y git
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dev
+sudo apt-get install -y libssl-dev
+
+# Docker-Compose
+sudo apt-get install libffi-dev
+sudo pip install docker-compose
+
+# Install Docker
+wget https://get.docker.com/ --output-document get-docker.sh
+sudo chmod +x get-docker.sh
+sudo ./get-docker.sh
