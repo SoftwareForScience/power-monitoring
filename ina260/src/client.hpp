e8845ef3bb51daac762da052fb35576c98786f5d
S. Schokker <stefan.schokker@hva.nl>
2019-11-21 12:19:58 +0100

    non modulair server client temp set up working. Starting now on modulair setup.

--- ina260/src/client.hpp
+++ ina260/src/client.hpp
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,7 +13,6 @@
 #include <cstring>
 #include <iterator>
 #include <map>
-#include <vector>
 
 #define DEGREE 9
 
@@ -23,22 +21,15 @@ using namespace std;
 class Client {
 
 public:
-    void Error(const char *msg);
-    //std::map<char *, char *>  Connect(char *argvv[]);
-    void Connect();
-    void Command(int argcc, char *argvv[]);
-    bool Stop();
-    void Init();
-    char *pi_temp_zero;
-    char *pi_temp_one;
-    char *pi_temp_two;
-    char *pi_temp_three;
-    char *pi_temp_g;
-    //map<char *, char *> temperatures;
+    void Error(const char *msg);                            //error function
+    void Call(int argcc, char *argvv[]);                    //Function to ask server pi's for data
+    map<std::string, std::string> Mapping(char *argvv[]);   //Function that returns map of data
+    void Init();                                            //Start by asking data pi its hostname
 
 private:
 
     int pies;
+    bool pass = false;
 
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
@@ -53,17 +44,8 @@ private:
 
     char templogs[sizeof(pies)][DEGREE];
 
-    char pi_zero[DEGREE];
-    char pi_one[DEGREE];
-    char pi_two[DEGREE];
-    char pi_three[DEGREE];
-    char pi_g[DEGREE];
-
     std::map<std::string, std::string> m;
     std::pair<std::string, std::string> p;
-
-
-    //map<char [], char []> temps;
 
     string localhost;
     string host;
