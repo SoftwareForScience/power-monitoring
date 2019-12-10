c2fa6f8c3e170e55ee4c448dd1225abf8971936f
S. Schokker <stefan.schokker@hva.nl>
2019-12-10 12:54:13 +0100

    added silkscreen layer pcb
    logos added and descriptions of terminals
    also added i2c pullups and alive LED

---
+++ ina260/src/inaDevice.hpp
@@ -0,0 +1,21 @@
+#ifndef INA260_INADEVICE_HPP
+#define INA260_INADEVICE_HPP
+
+#include "I2CDevice.hpp"
+#include "INASettings.hpp"
+#include <cstdint>
+
+class inaDevice : I2CDevice
+{
+public:
+	explicit inaDevice(int id);
+
+	double getShuntCurrent();
+	double getBusVoltage();
+	double getPower();
+private:
+	static uint16_t invert(uint16_t a);
+};
+
+
+#endif //INA260_INADEVICE_HPP
---
+++ ina260/src/logger.hpp
@@ -0,0 +1,51 @@
+//
+// Created by Stefan Schokker on 2019-11-14.
+//
+
+#ifndef INA260_LOGGER_HPP
+#define INA260_LOGGER_HPP
+
+#include <map>
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <iostream>
+#include <thread>
+#include <utility>
+#include <zconf.h>
+#include <chrono>
+#include "inaDevice.hpp"
+#include "server.hpp"
+#include "../lib/pugixml/src/pugixml.hpp"
+#include "../lib/date/include/date/date.h"
+
+namespace fs = std::filesystem;
+
+class logger
+{
+public:
+	explicit logger(fs::path outputDir, const fs::path &config, int port);
+
+	void startMea();
+	void stopMea();
+private:
+	struct piContainer
+	{
+		std::string name;
+		inaDevice *ina;
+		std::ofstream file;
+	};
+
+	void run();
+	void loadConfig(fs::path piXML);
+
+	bool stop = false;
+	std::thread* thread;
+	Server server_ = Server();
+
+	fs::path outDir, runDir;
+	std::vector<piContainer> nodes;
+};
+
+
+#endif //INA260_LOGGER_HPP
